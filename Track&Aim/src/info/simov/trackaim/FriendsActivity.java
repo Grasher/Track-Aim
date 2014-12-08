e999640ab1e472b39ed797e45db261f2378e9081
RitaNogueira <ana_rita_nogueira@hotmail.com>
2014-12-08 18:40:06 +0000

    Created space for searching a user

--- Track&Aim/src/info/simov/trackaim/FriendsActivity.java
+++ Track&Aim/src/info/simov/trackaim/FriendsActivity.java
@@ -4,12 +4,40 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FriendsActivity extends Activity {
+
+	private EditText userName;
+	private Button search;
+	private ListView userFoundList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_friends);
+		
+		userName = (EditText) findViewById(R.id.findUser);
+		userFoundList = (ListView) findViewById(R.id.usersList);
+		search = (Button) findViewById(R.id.search);
+		search.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				String name = userName.getText().toString();
+				Toast.makeText(getApplicationContext(), "User: " + name,
+						Toast.LENGTH_LONG).show();
+
+			}
+		});
+
 	}
 }
