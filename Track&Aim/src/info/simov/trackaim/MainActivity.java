cf4f65afaeecbf37794f2a31f2326275af116f68
RitaNogueira <ana_rita_nogueira@hotmail.com>
2014-12-09 22:59:24 +0000

    Created Camera Activity

--- Track&Aim/src/info/simov/trackaim/MainActivity.java
+++ Track&Aim/src/info/simov/trackaim/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends Activity implements OnClickListener,
 			@Override
 			public void onClick(View v) {
 				Intent i=new Intent(MainActivity.this,ChooseGroupActivity.class);
-				
+				startActivity(i);
 			}
 		});
 		
